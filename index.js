2dc10cf3807b0e0d4da378ad56389b488efc0b04
unknown <marisela.delgadillo.cbtis37@gmail.com>
2020-09-08 16:38:30 -0700

    Tacos server

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
 
 const port = 5000;
 
-app.use(bodyParser.json());//cosumir cosas
+app.use(bodyParser.json());//consumir cosas
+app.use(cors());
 
-const tacos = [
+let tacos = [
     {
         id: 1,
         name: 'de asada',
@@ -28,21 +30,58 @@ const tacos = [
     }
 ];
 
-
-//Metodo GET o Obtener
-app.get('/', (request, response) =>{
+//Metodo GET o Obtener READ
+app.get('/', (request, response) =>
+{
     response.send(tacos);
 });
 
-//agregar taco
-app.post('/', (request, response) => {
+app.get('/:id', (request, response) =>
+{
+    const {id} = request.params; //estos son los parametros
+    const taco = tacos.find(taco => taco.id == id);//en este taco => me vas a devolver como resultado cuando el id de ese taco sea igual como el que meti
+    response.send(taco);
+});
+
+// app.get('/:name', (request, response) =>
+// {
+//     const {name} = request.params;
+//     const taco = tacos.find(taco => taco.name == name);
+//     response.send(taco);
+// });
+
+//agregar taco CREATE
+app.post('/', (request, response) =>
+{
     const taco = request.body;
     const {name, quantity, pica} = request.body;
-    taco.id=tacos.length + 1;
+    taco.id = tacos.length + 1;
     tacos.push(taco);
     response.send(taco);
+});
 
-    //response.send({tag: `Llevate ${quantity} unidades por tan solo ${price}`});
+//el put actualiza cosas UPDATE
+app.put('/:id', (request, response) =>
+{
+    //buscamos lo que se va actualizar
+    const {id} = request.params;
+    const taco = tacos.find(taco => taco.id == id);
+    //pides los cambios al body
+    const {name, quantity, pica} = request.body;
+    //aplicar los cambios
+    taco.name = name;
+    taco.quantity = quantity;
+    taco.pica = pica;
+    //mostrar los cambios
+    response.send(taco);
+});
+
+//Borra DELETE
+app.delete ('/:id', (request, response)=>
+{
+    const {id} = request.params;
+    tacos = tacos.filter(taco=> taco.id !=id);
+    response.send(tacos);
 });
 
 app.listen(port, ()=> console.log(`Server started at: http://localhost:${port}`));
